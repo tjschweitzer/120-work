cf02ab8d0102ea27b273503aec26b8802aa95545
Trent Schweitzer <ts205169@umconnect.umt.edu>
2019-01-26 22:09:48 -0700

    Commented hw-3
    Added comments to hw-3

--- hw-3/sketch.js
+++ hw-3/sketch.js
@@ -1,16 +1,20 @@
 /*
+name: sketch.js
+Last Modified: 2019/1/26
 
-
+Author: T. Schweitzer
 */
+
+// Defines setup() function
 function setup(){
-
+// creates canvas size of 500px by 500 px
   createCanvas(500,500);
-//
+//Sets color of BG to 'Aqua'
   background("aqua");
-
 }
 
+// Defines draw(function)
 function draw() {
-  //
+  //Prints text at 250px,250px
   text("Hello World!",250,250);
 }
