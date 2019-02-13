8aec99a12f5ae03a656e29dde41ba9318602d760
Trent Schweitzer <ts205169@umconnect.umt.edu>
2019-02-13 00:26:37 -0700

    Final Projuect Hw -5
    Added read me and comments o ncode

--- hw-5/readme.md
+++ hw-5/readme.md
@@ -1,5 +1,17 @@
-# Empty p5 sketch.
+# Week 5 Review
 
-Please replace contents of this file with appropriate readme information after finishing your p5 sketch.
+## What Happened This Week
 
-Current p5 version -> 0.7.1
+  - This week we got in to what I am more comferable with when it comes to this class the coding portion
+  - After watching all the videos  and reading all the content for the week I dove in to the p5 resouce webpage to see what else I could learn and do for this project.
+  - When watching one of the videos they talked about conditional statements (If statements) to make the circle no just keep going after it got to the end of the screen. This make me think of the dvd screen saver that used to come up on most dvd players where it would change colors every time it hit a edge of the screen.
+
+
+ # Problems
+ ##### Oh so many where to start
+
+1. It took some thought initially to figure out how to make the square change directions when it reached any of the borders. Initially I used an if else if statement
+2. Then this statement didn’t have the desired effect when it hit the corner exactly so I had to break it up in to separate if statements in case it hit the exact corner
+3. Making the cube change colors wasn’t tricky but I had to figure out how to make it co through the majority of the spectrum and not just a single color This took one if statement for each color transition and a nested if statement for when if finished that color range and what was essentially a counter for each type of transition
+4. Then I decided to make the cube rotate Initially I tied this to frame count but I decided to tie the rotation to the mouse’s Y position so that needed to be recoded so the change in rotation speed was smooth
+5. Finally I tied the speed of the color rotation to the mouse’s X position I had to add a lot of extra handles in this so the variables for Red Green and Blue didn’t go outside of the 0-255 range.
--- hw-5/sketch.js
+++ hw-5/sketch.js
@@ -12,22 +12,37 @@ let colrR =255;
 let colrG = 255;
 let colrB =255;
 let colorCount =1;
+let colorSpeed = 1;
+let spinSpeed = 1;
+let spinCount = 0;
 
 function draw(){
 
-  // background(0); // Creates a Black background
-  sqrX = sqrX + moveX;
-  sqrY = sqrY + moveY;
-  fill(255);
-  text(colorCount,100,180);
-  text(colrR,100,200);
-  text(colrG,100,220);
-  text(colrB,100,240);
-  translate(sqrX,sqrY);
-  rotate(radians(frameCount));
-  stroke(255)
-  fill(colrR,colrG,colrB);
+  // Math Part - This is all of the incremental equations I used to create the effect
 
+  sqrX = sqrX + moveX;    // How the square moves the in x position
+  sqrY = sqrY + moveY;    // How the square moves i nthe Y position
+  colorSpeed = int(mouseX/ width *49) +1; //Takes the xposition of the mouse to infulence the  speed it changes color
+  spinSpeed = int(mouseY/ width *9) +1;   //Takes the position of the mouse to influence the speed it rotates
+  spinCount = spinCount + spinSpeed; // incrementally adds to the rotation of the square
+
+        //This was my code I used for debugging any issues with color or square position
+  // background(0);
+  // fill(255);
+  // text(colorSpeed,100,180);
+  // text(colrR,100,200);
+  // text(colrG,100,220);
+  // text(colrB,100,240);
+
+    //moves canvas to the next starting point for the square
+  translate(sqrX,sqrY);
+    //rotates the canvas before drawing the square
+  rotate(radians(spinCount));
+    //applys a white stoke
+  stroke(255)
+    //selects the color of the square
+  fill(colrR,colrG,colrB);
+    // Draws the square
   rect(0,0,40,40);
 
 // if the square reaches the left or right side it will change direction
@@ -43,70 +58,89 @@ function draw(){
       moveY = moveY * - 1;
   }
 
-// colrR 255-0 ,colrG 255, colrB 255  White to teal
+// This is my color progrssion system I came up with
+// colrR 255-0 ,colrG 255, colrB 255  White to Aqua
   if (colorCount == 1){
-    colrR = colrR -1;
-    if (colrR ==0){
+    colrR = colrR -colorSpeed;
+    //when var goes below 0 we set it to 0 and progress to next color transition
+    if (colrR <0){
       colorCount =2;
+      colrR = 0;
     }
   }
 
-  // colrR 0 , colrB 255 ,colrG 255-0 teal to Blue
+  // colrR 0 , colrB 255 ,colrG 255-0 Aqua to Blue
   if (colorCount == 2){
-    colrG = colrG -1;
-    if (colrG ==0){
+    colrG = colrG -colorSpeed;
+    //when var goes below 0 we set it to 0 and progress to next color transition
+    if (colrG <0){
       colorCount =3;
+      colrG=0;
     }
   }
 
-  // colrR 0-255 , colrB 255 ,colrG 0 blue to purple
+  // colrR 0-255 , colrB 255 ,colrG 0 Blue to Fuchsia
   if (colorCount == 3){
-    colrR = colrR +1;
-    if (colrR ==255){
+    colrR = colrR +colorSpeed;
+    //when var goes above 255 we set it to 255 and progress to next color transition
+    if (colrR >255){
       colorCount =4;
+      colrR = 255;
     }
   }
 
-  // colrR 255 , colrB 255-0 ,colrG 0 purple to red
+  // colrR 255 , colrB 255-0 ,colrG 0 Fuchsia to Red
   if (colorCount == 4){
-    colrB = colrB -1;
-    if (colrB ==0){
+    colrB = colrB -colorSpeed;
+    //when var goes below 0 we set it to 0 and progress to next color transition
+    if (colrB <0){
       colorCount =5;
+      colrB = 0;
     }
   }
 
 
-  // colrR 255 , colrB 0 ,colrG 0 red to yellow
+  // colrR 255 , colrB 0 ,colrG 0-255 Red to Yellow
   if (colorCount == 5){
-    colrG = colrG +1;
-    if (colrG ==255){
+    colrG = colrG +colorSpeed;
+    //when var goes above 255 we set it to 255 and progress to next color transition
+    if (colrG >255){
       colorCount =6;
+      colrG =255;
     }
   }
-  // colrR 255 , colrB 0 ,colrG 0 red to yellow
+  // colrR 255 , colrB 0 ,colrG 0 Yellow to Green
   if (colorCount == 6){
-    colrR = colrR -1;
-    if (colrR ==0){
+    colrR = colrR -colorSpeed;
+    //when var goes below 0 we set it to 0 and progress to next color transition
+    if (colrR <0){
       colorCount =7;
+      colrR=0;
     }
   }
-  // colrR 255 , colrB 0 ,colrG 0 red to yellow
+  // colrR 255 , colrB 0 ,colrG 0 Green to Black
   if (colorCount == 7){
-    colrG = colrG -1;
-    if (colrG ==0){
+    colrG = colrG -colorSpeed;
+    //when var goes below 0 we set it to 0 and progress to next color transition
+    if (colrG <0){
+      colrG=0;
       colorCount =8;
     }
   }
 
 
 
-  // colrR 255 , colrB 0 ,colrG 0 red to yellow
+  // colrR 0-255 , colrB 0-255 , colrG 0-255 Black to White
   if (colorCount == 8){
-    colrB = colrB +1;
-    colrR = colrR +1;
-    colrG = colrG +1;
-    if (colrG ==255){
+    colrB = colrB +colorSpeed;
+    colrR = colrR +colorSpeed;
+    colrG = colrG +colorSpeed;
+      // When one variable goes over 255 Sets all to 255 and gotes to starting color transition 
+    if (colrG >255){
       colorCount =1;
+        colrB = 255;
+        colrR = 255;
+        colrG = 255;
     }
   }
 
