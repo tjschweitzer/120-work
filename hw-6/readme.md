31eae5e8ef363f3da0f28f7cf6cdd668dd3cd725
Trent Schweitzer <ts205169@umconnect.umt.edu>
2019-02-21 04:41:01 -0700

    Added readme

--- hw-6/readme.md
+++ hw-6/readme.md
@@ -1,5 +1,7 @@
-# Empty p5 sketch.
+# Week 6
 
-Please replace contents of this file with appropriate readme information after finishing your p5 sketch.
+### Steps taken this Week
+ This week we covered a lot of tricks for coding that honestly would have made my project from week 5 a little easier. I was able to use the constrain method to my advantage to save a lot of space on coding. I do plan to take what I learned and recode my week 5 project to make the code cleaner
 
-Current p5 version -> 0.7.1
+ ### Problems
+ I ran in to an issue with just a personal lack of inspiration. I spent a lot of time trying and although I am not as satisfied with my project as I am my last few. I feel that this one at least gets better the longer it runs.
--- hw-6/sketch.js
+++ hw-6/sketch.js
@@ -37,6 +37,14 @@ tri4 = {
   side3x : 800,
   side3y : 600
 };
+tri5 = {
+  side1x: 400,
+  side1y: 300,
+  side2x: 400,
+  side2y : 300,
+  side3x : 400,
+  side3y : 300
+};
 
 
 
@@ -48,9 +56,10 @@ function draw(){
   // text( tri.side2x, 20, 160 );
   // text( tri.side3y , 20, 180 );
 
+
+//Top Left
     stroke(255,0,0);
     fill(255,255,0);
-
     triangle(tri1.side1x, tri1.side1y, tri1.side2x, tri1.side2y,tri1.side3x, tri1.side3y);
 
     tri1.side1x = constrain(tri1.side1x + random(-5,5),0,800);
@@ -60,9 +69,10 @@ function draw(){
     tri1.side3x = constrain(tri1.side3x + random(-5,5),0,800);
     tri1.side3y = constrain(tri1.side3y + random(-5,5),0,600);
 
+
+//Top Right
     stroke(0,255,0);
     fill(255,0,255);
-
     triangle(tri2.side1x, tri2.side1y, tri2.side2x, tri2.side2y,tri2.side3x, tri2.side3y);
 
     tri2.side1x = constrain(tri2.side1x + random(-5,5),0,800);
@@ -73,9 +83,9 @@ function draw(){
     tri2.side3y = constrain(tri2.side3y + random(-5,5),0,600);
 
 
+//Bottom Right
   stroke(222,87,95);
   fill(15,76,73);
-
   triangle(tri3.side1x, tri3.side1y, tri3.side2x, tri3.side2y,tri3.side3x, tri3.side3y);
 
   tri3.side1x = constrain(tri3.side1x + random(-5,5),0,800);
@@ -85,9 +95,10 @@ function draw(){
   tri3.side3x = constrain(tri3.side3x + random(-5,5),0,800);
   tri3.side3y = constrain(tri3.side3y + random(-5,5),0,600);
 
+
+//Bottom Left
   fill(124,70,129);
   stroke(109,163,176);
-
   triangle(tri4.side1x, tri4.side1y, tri4.side2x, tri4.side2y,tri4.side3x, tri4.side3y);
 
   tri4.side1x = constrain(tri4.side1x + random(-5,5),0,800);
@@ -98,6 +109,17 @@ function draw(){
   tri4.side3y = constrain(tri4.side3y + random(-5,5),0,600);
 
 
+//Middle Triangle
+  fill(128);
+  stroke(255);
+  triangle(tri5.side1x, tri5.side1y, tri5.side2x, tri5.side2y,tri5.side3x, tri5.side3y);
+
+  tri5.side1x = constrain(tri5.side1x + random(-5,5),0,800);
+  tri5.side1y = constrain(tri5.side1y + random(-5,5),0,600);
+  tri5.side2x = constrain(tri5.side2x + random(-5,5),0,800);
+  tri5.side2y = constrain(tri5.side2y + random(-5,5),0,600);
+  tri5.side3x = constrain(tri5.side3x + random(-5,5),0,800);
+  tri5.side3y = constrain(tri5.side3y + random(-5,5),0,600);
 
 
 }
