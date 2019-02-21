f7c63e413b8642ed07781892b7523195f1cff342
Trent Schweitzer <ts205169@umconnect.umt.edu>
2019-02-21 04:18:37 -0700

    4 colors added

--- hw-6/sketch.js
+++ hw-6/sketch.js
@@ -4,30 +4,100 @@ function setup(){
   frameRate(60);
 }
 
-var tri = {
-tri.side1x : 0,
-tri.side1y : 0,
-tri.side2x : 1,
-tri.side2y : 0,
-tri.side3x : 0,
-tri.side3y : 1
-}
+let tri;
+tri1 = {
+  side1x: 0,
+  side1y: 0,
+  side2x: 0,
+  side2y : 0,
+  side3x : 0,
+  side3y : 0
+};
+tri2 = {
+  side1x: 800,
+  side1y: 0,
+  side2x: 800,
+  side2y : 0,
+  side3x : 800,
+  side3y : 0
+};
+tri3 = {
+  side1x: 0,
+  side1y: 600,
+  side2x: 0,
+  side2y : 600,
+  side3x : 0,
+  side3y : 600
+};
+tri4 = {
+  side1x: 800,
+  side1y: 600,
+  side2x: 800,
+  side2y : 600,
+  side3x : 800,
+  side3y : 600
+};
 
 
 
 function draw(){
-  fill(255,255,00);
-  nostroke();
+  // text( tri.side1x, 20, 80 );
+  // text( tri.side1y , 20, 100 );
+  // text( tri.side2x, 20, 120 );
+  // text( tri.side2y , 20, 140 );
+  // text( tri.side2x, 20, 160 );
+  // text( tri.side3y , 20, 180 );
 
-  tri.side2x = tri.side2x + random(10);
-  tri.side2y = tri.side2y + random(10);
-  tri.side3x = tri.side3x + random(10);
-  tri.side3y = tri.side3y + random(10);
+    stroke(255,0,0);
+    fill(255,255,0);
+
+    triangle(tri1.side1x, tri1.side1y, tri1.side2x, tri1.side2y,tri1.side3x, tri1.side3y);
+
+    tri1.side1x = constrain(tri1.side1x + random(-5,5),0,800);
+    tri1.side1y = constrain(tri1.side1y + random(-5,5),0,600);
+    tri1.side2x = constrain(tri1.side2x + random(-5,5),0,800);
+    tri1.side2y = constrain(tri1.side2y + random(-5,5),0,600);
+    tri1.side3x = constrain(tri1.side3x + random(-5,5),0,800);
+    tri1.side3y = constrain(tri1.side3y + random(-5,5),0,600);
+
+    stroke(0,255,0);
+    fill(255,0,255);
+
+    triangle(tri2.side1x, tri2.side1y, tri2.side2x, tri2.side2y,tri2.side3x, tri2.side3y);
+
+    tri2.side1x = constrain(tri2.side1x + random(-5,5),0,800);
+    tri2.side1y = constrain(tri2.side1y + random(-5,5),0,600);
+    tri2.side2x = constrain(tri2.side2x + random(-5,5),0,800);
+    tri2.side2y = constrain(tri2.side2y + random(-5,5),0,600);
+    tri2.side3x = constrain(tri2.side3x + random(-5,5),0,800);
+    tri2.side3y = constrain(tri2.side3y + random(-5,5),0,600);
 
 
-  triangle(side1x,side1y,side2x,side2y,side3x,side3y);
+  stroke(222,87,95);
+  fill(15,76,73);
 
-  tri.side1x = tri.side3x;
-  tri.side1y = tri.side3y;
+  triangle(tri3.side1x, tri3.side1y, tri3.side2x, tri3.side2y,tri3.side3x, tri3.side3y);
+
+  tri3.side1x = constrain(tri3.side1x + random(-5,5),0,800);
+  tri3.side1y = constrain(tri3.side1y + random(-5,5),0,600);
+  tri3.side2x = constrain(tri3.side2x + random(-5,5),0,800);
+  tri3.side2y = constrain(tri3.side2y + random(-5,5),0,600);
+  tri3.side3x = constrain(tri3.side3x + random(-5,5),0,800);
+  tri3.side3y = constrain(tri3.side3y + random(-5,5),0,600);
+
+  fill(124,70,129);
+  stroke(109,163,176);
+
+  triangle(tri4.side1x, tri4.side1y, tri4.side2x, tri4.side2y,tri4.side3x, tri4.side3y);
+
+  tri4.side1x = constrain(tri4.side1x + random(-5,5),0,800);
+  tri4.side1y = constrain(tri4.side1y + random(-5,5),0,600);
+  tri4.side2x = constrain(tri4.side2x + random(-5,5),0,800);
+  tri4.side2y = constrain(tri4.side2y + random(-5,5),0,600);
+  tri4.side3x = constrain(tri4.side3x + random(-5,5),0,800);
+  tri4.side3y = constrain(tri4.side3y + random(-5,5),0,600);
+
+
+
 
 }
